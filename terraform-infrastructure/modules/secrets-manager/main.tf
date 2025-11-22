4102b968e00e6638a5aecb6fca73a03814288ea8
Ibrahim Yasser <ebrahim195016@feng.bu.edu.eg>
2025-11-22 17:52:12 +0200

    fix: modified the ingress to create one for vote and one for result

--- terraform-infrastructure/modules/secrets-manager/main.tf
+++ terraform-infrastructure/modules/secrets-manager/main.tf
@@ -315,6 +315,7 @@ resource "aws_iam_policy" "alb_controller" {
           "elasticloadbalancing:DescribeLoadBalancers",
           "elasticloadbalancing:DescribeLoadBalancerAttributes",
           "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerAttributes",
           "elasticloadbalancing:DescribeListenerCertificates",
           "elasticloadbalancing:DescribeSSLPolicies",
           "elasticloadbalancing:DescribeRules",
